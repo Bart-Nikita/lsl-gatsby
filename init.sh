4f48fe30fde6050d863d21c32ba21e88ea2582bb
Bart-Nikita <info@bart-group.com>
2023-09-27 17:46:27 +0800

    first commit

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://info@bart-group.com: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@github.com/Bart-Nikita/lsl-gatsby.git
+git remote add origin https://Bart-Nikita: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@github.com/Bart-Nikita/lsl-gatsby.git
 git push -u origin main
